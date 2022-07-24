01c1e4c79990ea57463aed0d84f2795bca7ebb06
B3r4ti0n <arcoucou77@gmail.com>
2022-07-24 21:57:04 +0200

    tk

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ height = tk.winfo_screenheight()
 
 width = tk.winfo_screenwidth()
 # Open website
-webview.create_window('GPS', 'https://www.google.com/maps/dir/?api=1&travelmode=driving', height=height, width=width)
-webview.start()
+#webview.create_window('GPS', 'https://www.google.com/maps/dir/?api=1&travelmode=driving', height=height, width=width)
+#webview.start()
 
 tk.mainloop()
