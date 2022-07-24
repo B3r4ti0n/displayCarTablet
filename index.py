02416d864f78c1708ce33dbee7fb102aac39145b
B3r4ti0n <arcoucou77@gmail.com>
2022-07-24 21:38:01 +0200

    first commit

--- index.py
+++ index.py
@@ -1,10 +1,16 @@
 from tkinter import *
-from tkinter import ttk
+import webview
 
+# define an instance of tkinter
+tk = Tk()
 
-windows = Tk()
-windows.attributes('-fullscreen', True)
-label = Label(windows, text="Hello World")
-label.pack()
+#  size of the window where we show our website
+tk.attributes('-fullscreen', True)
+height = tk.winfo_screenheight()
 
-windows.mainloop()
+width = tk.winfo_screenwidth()
+# Open website
+webview.create_window('GPS', 'https://www.google.com/maps/dir/?api=1&travelmode=driving', height=height, width=width)
+webview.start()
+
+tk.mainloop()
