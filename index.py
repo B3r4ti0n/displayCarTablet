765bb09bad46a03b438892c51bee2193d8d8519e
B3r4ti0n <arcoucou77@gmail.com>
2022-07-25 17:19:51 +0200

    .

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from src.gps_page import gps_page
+import webview
 
 # define an instance of tkinter
 tk = Tk()
@@ -8,7 +8,7 @@ width = tk.winfo_screenwidth()
 #  size of the window where we show our website
 #tk.attributes('-fullscreen', True)
 
-btn = Button(tk, text='GPS', command=lambda: gps_page(width, height, tk))
-btn.pack()
+#btn = Button(tk, text='GPS', command=lambda: gps_page(width, height, tk))
+#btn.pack()
 
 tk.mainloop()
