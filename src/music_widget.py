3db68ca0aa2afae9a562cc9ceef72072dcc7e143
B3r4ti0n <arcoucou77@gmail.com>
2022-07-26 18:42:17 +0200

    music test

--- src/music_widget.py
+++ src/music_widget.py
@@ -3,5 +3,5 @@ import subprocess
 
 
 def music(window=None):
-    test = f.create_button(window=window, text='test', command=lambda: subprocess.call(['alsamixer', '/'], shell=False))
+    test = f.create_button(window=window, text='test', command=lambda: subprocess.call(['alsamixer', '/'], shell=True))
     test.place(x = 500, y = 400)
